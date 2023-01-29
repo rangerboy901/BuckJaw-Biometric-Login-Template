a3cb4fb147283de06c93857edb10756179d05f3f
rangerboy574 <jdeweese574@gmail.com>
2023-01-29 09:50:31 -0600

    Initial Commit

---
+++ BuckJaw Biometric Login Template/BuckJaw_Biometric_Login_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuckJaw_Biometric_Login_TemplateApp.swift
+//  BuckJaw Biometric Login Template
+//
+//  Created by Joseph  DeWeese on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BuckJaw_Biometric_Login_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
